3947cdebb8ff6771b41377ff0ad8a3f85f45d585
bernabeiluca-gif <bernabei.luca@franchettisalviani.net>
2025-11-06 02:06:19 +0100

    Modifica1

--- EserciziArray/Program.cs
+++ EserciziArray/Program.cs
@@ -40,6 +40,45 @@
                     Console.Write("[" +  numeriDispari[i] + "]");   
                 }
             }
+            // es6
+            int[] vettore1 = { 4, 8, 6, 5, 9, 7 };
+            int x = 7, contamaggiore = 0, contaminore = 0;
+            for (int i=0;  i< vettore1.Length; i++)
+            {
+                if(vettore1[i]<x)
+                {
+                    contaminore = contamaggiore + 1;
+                }
+                else
+                {
+                    contamaggiore = contamaggiore + 1;
+                }
+            }
+            int[] vettore2 = new int[contaminore];
+            int[] vettore3 = new int[contamaggiore];
+            int indicemaggiore = 0, indiceminore = 0;
+            for(int i=0;i< vettore1.Length;i++)
+            {
+                if(vettore1[i]<x)
+                {
+                    vettore2[indiceminore] = vettore1[i];
+                    indiceminore = indiceminore + 1;
+                }
+                else
+                {
+                    vettore3[indicemaggiore] = vettore1[i];
+                    indicemaggiore = indicemaggiore + 1;
+                }
+            }
+            for(int i=0;i<=vettore2.Length;i++)
+            {
+                Console.Write($"[{vettore2[i]}]");
+            }
+            Console.WriteLine();
+            for(int i=0; i<=vettore3.Length;i++)
+            {
+                Console.Write($"[{vettore3[i]}]");
+            }
         }
     }
 }
